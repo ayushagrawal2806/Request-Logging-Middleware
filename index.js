26ffc8a8a750c98a1d23c915172b24d1214323ae
ayushagrawal <ayushagrawal6082@gmail.com>
2024-05-18 16:38:28 +0530

    Done

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const fs = require("fs");
+
+let loggingMiddleware = (req, res, next) => {
+  fs.appendFileSync(
+    "requestlog.log",
+    `Request received in ${req.url} with ${req.method} method from ${
+      req.ip
+    } at ${new Date()} \n`
+  );
+  next();
+};
+
+const app = express();
+
+app.use(loggingMiddleware);
+
+app.get("/products", (req, res) => {
+  res.status(200).json({
+    message: "Dummy product json data",
+  });
+});
+
+app.use("/*", (req, res) => {
+  res.status(400).json({
+    error: "Path not found",
+  });
+});
+
+app.listen(8080, () => {
+  console.log("Server is running at port -", 8080);
+});
